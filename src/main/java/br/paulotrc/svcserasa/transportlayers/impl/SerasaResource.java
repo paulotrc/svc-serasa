6089afea465a86b505abdb4b34d3473686dec067
paulotrc <paulotrc@gmail.com>
2023-02-09 11:50:03 -0300

    [UPDATE] Project Configs.

--- src/main/java/br/paulotrc/svcserasa/transportlayers/impl/SerasaResource.java
+++ src/main/java/br/paulotrc/svcserasa/transportlayers/impl/SerasaResource.java
@@ -10,7 +10,6 @@ import br.paulotrc.svcserasa.transportlayers.dto.response.SerasaResponse;
 import br.paulotrc.svcserasa.transportlayers.mappers.SerasaMapper;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
